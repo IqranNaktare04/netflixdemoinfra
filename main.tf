09ae801c5fc8115a19112f8a61a265ba08c56140
IqranNaktare04 <devopsbyiqran04@gmail.com>
2025-04-24 15:58:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-080b1a55a0ad28c02"
+ami = "ami-091dccf4e2d272bae"
 instance_type = "t2.medium"
 key_name = "netflix"
 vpc_security_group_ids = ["sg-0af37b58629133ca9"]
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["Ansible", "dev-1", "dev-2", "Test-1", "Test-2", "Monitoring server"]
 }
