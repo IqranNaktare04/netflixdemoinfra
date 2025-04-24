43781689e56aeca2531f8ce6270973076e90ce62
IqranNaktare04 <devopsbyiqran04@gmail.com>
2025-04-24 16:12:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 6
 ami = "ami-091dccf4e2d272bae"
 instance_type = "t2.medium"
-key_name = "netflix"
+key_name = "IQN"
 vpc_security_group_ids = ["sg-0af37b58629133ca9"]
 tags = {
 Name = var.instance_names[count.index]
